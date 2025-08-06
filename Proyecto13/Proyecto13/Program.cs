7ef303a72d24d05b3f0f16706b281a583eea0d68
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-06 16:05:31 -0400

    Ejercicio 13 terminado

--- Proyecto13/Proyecto13/Program.cs
+++ Proyecto13/Proyecto13/Program.cs
@@ -10,6 +10,31 @@ namespace Proyecto13
     {
         static void Main(string[] args)
         {
+            //Problema propuesto
+            //Se ingresa por teclado un número positivo de uno o dos dígitos(1..99) mostrar un mensaje
+            //indicando si el número tiene uno o dos dígitos.
+            //(Tener en cuenta que condición debe cumplirse para tener dos dígitos, un número entero)
+
+            int numero;
+
+            Console.Write("Ingrese el numero para saber si es de 1 digito o de 2 digitos: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 10 && numero <= 99)
+            {
+                Console.WriteLine("El numero " + numero + " es un numero de 2 digitos");
+
+            }else if (numero <=9)
+            {
+                Console.WriteLine("El numero " + numero + " es un numero de 1 digito");
+            }
+            else
+            {
+                Console.WriteLine("Es un numero mayor a 2 digitos.");
+                
+            }
+            Console.ReadKey();
+
         }
     }
 }
