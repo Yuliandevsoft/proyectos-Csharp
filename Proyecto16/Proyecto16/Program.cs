d7c8df32c30de0f7e540723e61c3f1bec8c980fa
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-07 15:24:50 -0400

    Proyecto 16 terminado

--- Proyecto16/Proyecto16/Program.cs
+++ Proyecto16/Proyecto16/Program.cs
@@ -10,6 +10,30 @@ namespace Proyecto16
     {
         static void Main(string[] args)
         {
+            //Problema propuesto
+            //Se ingresa por teclado un valor entero, mostrar una leyenda que 
+            //indique si el número es positivo o negativo y nulo si es 0 .
+
+            int num;
+            Console.Write("Ingrese un valor para mostrar una leyenda: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num == 0) {
+                Console.WriteLine("Es nulo");
+            }
+            else
+            {
+                if (num < 1)
+                {
+                    Console.WriteLine("Es negativo");
+                }
+                else
+                {
+                    Console.WriteLine("Positivo");
+                }
+            }
+
+
         }
     }
 }
