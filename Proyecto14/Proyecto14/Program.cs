212a4815c60b2b9be0ae0c9005d5893a899c25b5
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-07 10:57:52 -0400

    Proyecto14 de notas con estructuras condicionales anidadas 1

--- Proyecto14/Proyecto14/Program.cs
+++ Proyecto14/Proyecto14/Program.cs
@@ -10,6 +10,40 @@ namespace Proyecto14
     {
         static void Main(string[] args)
         {
+            int nota1, nota2, nota3, promedio;
+
+            Console.Write("Ingrese la primera nota: ");
+            nota1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la segunda nota: ");
+            nota2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la tercera nota: ");
+            nota3 = int.Parse(Console.ReadLine());
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+             if(promedio >= 7)
+            {
+                Console.WriteLine("Promocionado");
+
+            }
+            else
+            {
+                if (promedio >= 4)
+                {
+                    Console.WriteLine("Regular");
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+            Console.ReadKey();
+                    
+
+
+
         }
     }
 }
