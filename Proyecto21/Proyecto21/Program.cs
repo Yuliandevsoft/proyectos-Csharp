41accd5a4dafe214237e1ba78a1c08a361561910
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-08 14:32:15 -0400

    Condiciones compuestas con operadores lógicos 3

---
+++ Proyecto21/Proyecto21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condiciones compuestas con operadores lógicos
+            //Problema propuesto
+            //Realizar un programa que pida cargar una fecha cualquiera, luego verificar si dicha fecha
+            //corresponde a Navidad.
+
+            int dia, mes, year;
+
+            Console.Write("Ingrese el dia: ");
+            dia =  int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el mes: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el año: ");
+            year = int.Parse(Console.ReadLine());
+
+            if (dia == 25 && mes == 12) {
+
+                Console.WriteLine("Dia: " + dia + " Mes: " + mes + " Año: " + year);
+                Console.WriteLine("Ya hoy es navidad!!!");
+            }
+            else
+            {
+                Console.WriteLine("Dia: "+ dia + " Mes: "+ mes + " Año: "+ year);
+                Console.WriteLine("Aun no es navidad");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
