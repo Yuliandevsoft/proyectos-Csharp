15b97cbe1d7ffe90253410c930802d901b8ab935
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-09 13:02:40 -0400

    Estructura repetitiva While, ejercicio 3

---
+++ Proyecto30/Proyecto30/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema 3:
+            //Desarrollar un programa que permita la carga de 10 valores por teclado y nos muestre
+            //posteriormente la suma de los valores ingresados y su promedio.
+
+            int x, valor, promedio, suma;
+            x = 1;
+            suma = 0;
+           
+            while (x <= 10) {
+
+                Console.Write("Ingrese el valor del contador: ");
+                valor = int.Parse(Console.ReadLine());
+                suma = (suma + valor);
+                x++;
+
+            }
+            promedio = suma / 10 ;
+            Console.WriteLine("Esta es la suma de los valores ingresados: " + suma);
+            Console.WriteLine("El promedio es: " +promedio);
+            Console.ReadKey();
+
+        }
+        
+    }
+}
