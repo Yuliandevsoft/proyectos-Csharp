926ba888f8b2ea56f4d1ee58d4c80b2555fb92cc
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-11 11:40:12 -0400

    Estructura repetitiva while, ejercicio 6

---
+++ Proyecto33/Proyecto33/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema propuesto
+            //Se ingresan un conjunto de n alturas de personas por teclado.Mostrar la altura promedio
+            //de las personas.
+
+            int x = 1;
+            int n;
+            float altura, promedio;
+            float suma = 0;
+
+            Console.Write("Ingrese la cantidad de personas: ");
+            n = int.Parse(Console.ReadLine());
+
+            while (x <= n)
+            {
+                Console.Write("Ingrese la altura" + x + ": ");
+                altura= float.Parse(Console.ReadLine());
+                suma = suma + altura;
+                
+                x++;
+            }
+            promedio = suma / n;
+            Console.WriteLine("El promedio de las alturas ingresadas es este: " + promedio);
+            Console.ReadKey();
+
+
+        }
+    }
+}
