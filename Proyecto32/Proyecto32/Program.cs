b8460706d8ba1012a94a11629b12d47ad89759f7
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-11 10:52:01 -0400

    Estructura repetitiva while, ejercicio 5

---
+++ Proyecto32/Proyecto32/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Problema propuesto
+            //Escribir un programa que solicite ingresar 10 notas de alumnos y nos informe cuántos
+            //tienen notas mayores o iguales a 7 y cuantos menores.
+
+            int x = 1;
+            int notasMayores = 0;
+            int notasMenores = 0;
+            int nota;
+
+            while (x <= 10)
+            {
+                Console.Write("Ingrese la nota del alumno " + x + ": ");
+                nota = int.Parse(Console.ReadLine());
+
+                if (nota >= 7)
+                {
+                    notasMayores++;
+                }
+                else
+                {
+                    notasMenores++;
+                }
+                x++;
+            }
+
+            Console.WriteLine("Notas mayores o iguales a 7: " + notasMayores);
+            Console.WriteLine("Notas menores a 7: " + notasMenores);
+            Console.ReadKey();
+        }
+    }
+}
