47aa97aa1ea8e3f7864c4e4c01956f46cff70121
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-11 14:19:13 -0400

    Estructura repetitiva while, ejercicio 8

---
+++ Proyecto35/Proyecto35/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema propuesto
+            //Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. (No se
+            //ingresan valores por teclado)
+
+            int x = 1;
+            int serie = 11;
+
+
+            while (x <= 25)
+            {
+                Console.Write(serie);
+                Console.Write(" - ");
+                serie = serie +11;
+                x++;
+            }
+           
+            Console.ReadKey();
+
+        }
+    }
+}
