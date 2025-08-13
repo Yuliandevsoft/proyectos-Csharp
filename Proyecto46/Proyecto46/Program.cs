1a6ca6f1c60463dab9978ef25d426cdc8fc709e1
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-13 12:05:14 -0400

    Estructura repetitiva for, ejercicio 8

---
+++ Proyecto46/Proyecto46/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema propuesto
+            //Desarrollar un programa que muestre la tabla de multiplicar del 5(del 5 al 50) 
+
+            int tabla = 5, operacion;
+
+            Console.WriteLine("Tabla de multiplicar del 5: ");
+            Console.WriteLine("");
+
+            for (int i = 1; i <= 50; i++)
+            {
+                operacion = tabla * i;
+                Console.WriteLine($"5 x {i} = " + operacion);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
