55e9468f5d4af17a2a8d9ebd66b3c6b690054cf0
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-18 11:51:36 -0400

    Cadenas de caracteres problema 2

---
+++ Proyecto58/Proyecto58/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Problema 2:
+            //Solicitar el ingreso de dos nombres de personas. Mostrar un mensaje si son iguales o distintos.
+
+            string nombre, nombre2;
+            Console.WriteLine("Ingrese el primer nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el segundo nombre: ");
+            nombre2 = Console.ReadLine();
+
+            if (nombre != nombre2)
+            {
+                Console.WriteLine("Son nombres distintos");
+            }
+            else
+            {
+                Console.WriteLine("Sonn nombres iguales");
+            }
+            Console.ReadKey();
+        }
+    }
+}
