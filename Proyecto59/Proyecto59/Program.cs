240be6ea5838dd7ca272d12df8033f55341c02ad
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-19 10:50:56 -0400

    Declaración de una clase y definición de objetos 1, introduccion metodo POO

--- Proyecto59/Proyecto59/Program.cs
+++ Proyecto59/Proyecto59/Program.cs
@@ -8,8 +8,48 @@ namespace Proyecto59
 {
     internal class Persona
     {
+        private string nombre;
+        private int edad;
+
+        public void Inicializar()
+        {
+            Console.Write("Ingrese el nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese la edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+        }
+
+        public void Impirmir()
+        {
+            Console.WriteLine("--------------- Datos de la persona ---------------");
+            Console.WriteLine("");
+            Console.Write("Nombre: ");
+            Console.WriteLine(nombre);
+            Console.Write("Edad: ");
+            Console.WriteLine(edad);
+        }
+
+        public void EsMayorEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine(nombre + " es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine(nombre + " no es mayor de edad");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Persona persona1 = new Persona();
+            persona1.Inicializar();
+            persona1.Impirmir();
+            persona1.EsMayorEdad();
+            Console.ReadKey();
         }
     }
-}
+}  
+ 
