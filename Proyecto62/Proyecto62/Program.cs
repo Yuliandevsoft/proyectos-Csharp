b5387946e4bf3480f618b55eb4978e1ceabee074
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-19 16:49:03 -0400

    Declaración de una clase y definición de objetos ejercicio 4

---
+++ Proyecto62/Proyecto62/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problema 4:
+//Desarrollar una clase que represente un Cuadrado y tenga los siguientes métodos: cargar el valor de
+//su lado, imprimir su perímetro y su superficie.
+
+namespace Proyecto62
+{
+    internal class Cuadrado
+    {
+        private int lado;
+
+        public void Inicializar()
+        {
+            Console.WriteLine("Ingresa el valor del cuadrado: ");
+            lado = int.Parse(Console.ReadLine());
+            Perimetro();
+            Area();
+        }
+
+        public void Perimetro()
+        {
+            Console.WriteLine("Su perimetro es: " + lado * 4);
+        }
+
+        public void Area()
+        {
+            Console.WriteLine("Su Área es: " + lado * lado);
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Cuadrado cuadradito = new Cuadrado();
+            cuadradito.Inicializar();
+            Console.ReadKey();
+        }
+    }
+}
+
