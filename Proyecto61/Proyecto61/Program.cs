2587f9cdb5f42770c9c6505966c9474c88d68dc5
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-19 16:18:51 -0400

    Declaración de una clase y definición de objetos ejercicio 3

---
+++ Proyecto61/Proyecto61/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problema 3:
+//Desarrollar una clase que represente un punto en el plano y tenga los siguientes métodos: cargar los
+//valores de x e y, imprimir en que cuadrante se encuentra dicho punto (concepto matemático, primer
+//cuadrante si x e y son positivas, si x<0 e y>0 segundo cuadrante, etc.)
+ 
+namespace Proyecto61
+{
+    internal class PlanoCartesiano
+    {
+        private int x, y;
+
+        public void Inicializar()
+        {
+            do
+            {
+                Console.WriteLine("-----Ingrese los puntos del plano (999 para cerrar)-----");
+                Console.WriteLine("");
+                Console.Write("Ingrese el valor de X: ");
+                x = int.Parse(Console.ReadLine());
+                if (x != 999)
+                {
+                    Console.Write("Ingrese el valor de Y: ");
+                    y = int.Parse(Console.ReadLine());
+                    Cuadrantes();
+                    Console.WriteLine("");
+
+                }
+           
+            } while (x != 999);
+
+        }
+
+        public void Cuadrantes()
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("--Se encuentra en el primer cuadrante");
+            }
+            else
+            {
+                if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("--Se encuentra en el segundo cuadrante");
+                }
+                else
+                {
+                    if (x < 0 && y < 0)
+                    {
+                        Console.WriteLine("--Se encueentra en el tercer cuadrante");
+                    }
+                    else
+                    {
+                        if (x > 0 && y < 0)
+                        {
+                            Console.WriteLine("--Se encuentra en el cuarto cuadrante");
+                        }
+                        else
+                        {
+                            Console.WriteLine("--Se encuentra en el origen del plano");
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            PlanoCartesiano plano = new PlanoCartesiano();
+            plano.Inicializar();
+            //plano.Cuadrantes();
+            Console.WriteLine("Presiona enter 2 veces para cerrar");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Proyecto61/Proyecto61/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Proyecto61")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proyecto61")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("92f0bea8-c48d-428f-bfba-463a731d7a78")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
