d754851dd660beab967f633b779b10be1030a19b
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-20 17:08:24 -0400

    Declaración de métodos 1

---
+++ Proyecto65/Proyecto65/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problema 1:
+//Confeccionar una clase que permita ingresar valores enteros por teclado y nos muestre la tabla de
+//multiplicar de dicho valor. Finalizar el programa al ingresar el -1.
+
+namespace Proyecto65
+{
+    internal class TablaMultiplicar
+    {
+        private int valor;
+        public void CargarValor()
+        {
+            do
+            {
+                Console.WriteLine("Ingresa el valor a mostrar en tabla (-1 para finalizar programa): ");
+                valor = int.Parse(Console.ReadLine());
+                if (valor != -1)
+                {
+                    Calcular(valor);
+                }
+
+            } while (valor != -1);
+
+        }
+            public void Calcular(int v)
+        {
+            for (int n = 1; n <= 12; n++) 
+                for (int i = v; i <= v * 12; i = i + v)
+            {
+
+                Console.Write(n + " x " + n + " = " + i);
+                //Console.WriteLine(" = " + i);
+            }
+            Console.WriteLine();
+
+        }
+        
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tablaMul1 = new TablaMultiplicar();
+            tablaMul1.CargarValor();
+            Console.ReadKey();
+        }
+    }
+} 
---
+++ Proyecto65/Proyecto65/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Proyecto65")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proyecto65")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0d87ebd-327f-421c-9f6e-d0c50ffdf563")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
