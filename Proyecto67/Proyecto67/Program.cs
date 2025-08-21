5c6a0eabf70020f39fd908c66d9338e2cd445ac8
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-21 11:46:15 -0400

    Estructura de datos tipo vectot ejercicio 1

---
+++ Proyecto67/Proyecto67/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problema 1:
+//Se desea guardar los sueldos de 5 operarios.
+//Según lo conocido deberíamos definir 5 variables si queremos tener en un cierto momento los 5
+//sueldos almacenados en memoria.
+//Empleando un vector solo se requiere definir un único nombre y accedemos a cada elemento por
+//medio del subíndice.
+
+namespace Proyecto67
+{
+    internal class SueldoOperarios
+    {
+        private int[] sueldo;
+
+        public void CargarOperarios()
+        {
+            sueldo = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese el sueldo {i + 1 } : ");
+                sueldo[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Mostrar();
+        }
+         
+        public void Mostrar()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Los sueldos son" + sueldo[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            SueldoOperarios sueldo1 = new SueldoOperarios();
+            sueldo1.CargarOperarios();
+            Console.ReadKey();
+
+        }
+    }
+}
