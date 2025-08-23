f4b7852d6b14d6cf0362c05f2de51c9c3c146dfd
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-08-23 13:11:13 -0400

    Subiendo

---
+++ Proyecto73/Proyecto73/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problema propuesto
+//Cargar un vector de 10 elementos y verificar posteriormente si el mismo esta ordenado de
+//menor a mayor.
+
+namespace Proyecto73
+{
+    internal class ElementosMeMa
+    {
+        private int[] elemento;
+        private int[] menores;
+
+        private void CargarElementos()
+        {
+            elemento = new int[10];
+            menores = new int[10];
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write($"Ingresa el valor {i + 1}: ");
+                while(!int.TryParse(Console.ReadLine(), out elemento[i]))
+                {
+                    Console.WriteLine("Valor invalido, Ingrese un valor numerico valido: ");
+                }
+            }
+            for(int i = 0;i < 10; i++)
+            {
+                if (elemento[i] < menores[+i])
+                {
+                    Console.WriteLine(elemento[i]);
+                }
+            }
+           
+            
+            
+
+        }
+        static void Main(string[] args)
+        {
+            ElementosMeMa elemema = new ElementosMeMa();
+            elemema.CargarElementos();
+
+            Console.ReadKey();
+        }
+    }
+}
